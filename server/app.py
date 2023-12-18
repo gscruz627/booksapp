09d128202d9fda4586b8f17e8b12008689b0fe4a
Gustavo <gscruz627@protonmail.com>
2023-12-18 10:36:27 -0600

    gunicorn 2

--- server/app.py
+++ server/app.py
@@ -1,21 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 from db import db
-app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mnuihgeo:7HmG4WxWR5U8E2TPH12NhwwxfTeOUPGM@berry.db.elephantsql.com/mnuihgeo"
-db.init_app(app)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
 from routing.authroutes import auth as auth_blueprint
 from routing.mainroutes import main as main_blueprint
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(main_blueprint)
+from models import Book, User
 
-from models import Book,User
-with app.app_context():
-    db.create_all()
-
-if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mnuihgeo:7HmG4WxWR5U8E2TPH12NhwwxfTeOUPGM@berry.db.elephantsql.com/mnuihgeo"
+    db.init_app(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    app.register_blueprint(auth_blueprint)
+    cors = CORS(app)
+    app.register_blueprint(main_blueprint)
+    with app.app_context():
+        db.create_all()
+    return app
