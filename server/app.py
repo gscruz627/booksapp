f30cb3a3c8014eb1306900e89c8e45f222e23099
Gustavo <gscruz627@protonmail.com>
2023-12-18 18:42:08 -0600

    Security update 2

--- server/app.py
+++ server/app.py
@@ -8,6 +8,7 @@ from os import getenv
 
 def create_app():
     postgreslink = getenv("POSTGRES_LINK")
+    print(postgreslink)
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = str(postgreslink)
     db.init_app(app)
